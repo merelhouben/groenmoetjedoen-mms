a95f8ca84c805f1d3a4864e970d8a823944c54c4
merelhouben <70858955+merelhouben@users.noreply.github.com>
2022-01-28 13:37:47 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,40 +2,6 @@ new fullpage('#fullpage', {
     autoScrolling: true
 });
 
-// (window).scroll(function () {
-//     $(".section").each(function (index) {
-        
-
-//             newIndex = index + 1;
-//             volgendeScene = newIndex + 1;
-//             vorigeScene = newIndex - 1;
-
-//             console.log(newIndex);
-
-        
-//     });
-
-    // $('#bol1').click(function () {
-
-    //     if (newIndex == $(".section").length) {
-    //         $('html').animate({
-    //             scrollTop: $('#zon1').offset().top
-    //         });
-    //     } else {
-    //         $('html').animate({
-    //             scrollTop: $('#zon' + volgendeScene).offset().top
-    //         });
-    //     }
-    // });
-    
-
-// $('#bol1').click(function () {
-
-//     if(document.getElementById('bol1').clicked == true){
-        
-//     }
-// });
-
 $('.extra-info').click(function () {
     if ($('.extra-info').hasClass('info')) {
         $('.extra-info').removeClass('info');
@@ -45,10 +11,3 @@ $('.extra-info').click(function () {
     }
 })
 
-// $('#bol1').click(function () {
-//     if (newIndex == $('.section').length) {
-//         $('html').animate({
-//             scrollTop: $('#zon1' + volgendeScene).offset().top
-//         });
-//     }
-// })
