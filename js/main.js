6aac39d29aca8b88378ca289bb28fa9f24d97d1b
merelhouben <70858955+merelhouben@users.noreply.github.com>
2022-01-28 20:53:37 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+new fullpage('#fullpage', {
+    autoScrolling: true
+});
+
+
+
+// $('.extra-info').click(function () {
+//     if ($('.extra-info').hasClass('info-zon')) {
+//         $('.extra-info').removeClass('info-zon');
+//         $('#info-zon').remove();
+
+//     } else {
+//         $('.extra-info').addClass('info-zon');
+//         $.ajax({
+//             url: 'info-zonnenpanelen.html',
+        
+//             success: function (data) {
+//                 console.log(data);
+        
+//                 $('.extra-info').append('<div id="info-zon">' + data + '</div>');
+                
+//             }
+//         });
+//     }
+// });
+
+
+
